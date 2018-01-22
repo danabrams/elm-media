5560ea4e5faba6a30fd8232e21ffdd36da63d31d
Daniel Abrams <danabrams@Daniels-MacBook.local>
2018-01-22 14:56:17 -0500

    few small updates. Still setting up environment

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,6 @@
 module Main exposing (..)
 
-import Html exposing (Html, text, div, h1, img)
+import Html exposing (Html, div, h1, img, text)
 import Html.Attributes exposing (src)
 
 
@@ -36,9 +36,7 @@ update msg model =
 view : Model -> Html Msg
 view model =
     div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
-        ]
+        []
 
 
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
-import './main.css';
 import { Main } from './Main.elm';
-import registerServiceWorker from './registerServiceWorker';
 
 Main.embed(document.getElementById('root'));
 
-registerServiceWorker();
